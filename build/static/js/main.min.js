a8a41b6d3c17a795c682344c8b95a3432e55bd75
terehov <terehov79@gmail.com>
2023-01-24 17:37:15 +0200

    adaptive

--- build/static/js/main.min.js
+++ build/static/js/main.min.js
@@ -24,12 +24,39 @@
   new Swiper('.history__specialists .swiper', {
     speed: 500,
     spaceBetween: 0,
-    slidesPerView: 6,
+    slidesPerView: 2,
     loop: true,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+    // when window width is >= 768px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 0
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 0
+    },
+    // when window width is >= 1366px
+    1366: {
+      slidesPerView: 4,
+      spaceBetween: 0
+    },
+    // when window width is >= 1400px
+    1400: {
+      slidesPerView: 6,
+      spaceBetween: 0
+    }
+    }   
   });
+
+  $('.burger').on('click', function () {
+    $('.header__menu').toggleClass('active');
+    $(this).toggleClass('active');
+  })
 
 })(jQuery);
